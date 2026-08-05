cd9c6fb13dcfd890c09c630ce129ee1b4c8c8c4f
emilywhitty224-ctrl <emilywhitty224-ctrl@users.noreply.github.com>
2026-08-05 17:04:06 +0000

    deploy: a0c8063e20992069477b0551eb11719753ad06b0

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Smokemon/sw.js', { scope: '/Smokemon/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
